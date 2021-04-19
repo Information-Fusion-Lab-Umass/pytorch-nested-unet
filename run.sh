7f7cd72ac7d4fca347d708d428f960e51b1efc31
vbhave <vbhave@umass.edu>
2021-04-19 12:09:44 -0400

    Unnecessary parameters in script removed.

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python train.py --dataset ax_crop_dataset --dataset2 fa_dataset --arch MultiViewNestedUNet --img_ext .png --mask_ext .png --num_classes=2 --input_w=96 --input_h=96 --batch_size=16
+python train.py --dataset ax_crop_dataset --dataset2 fa_dataset --arch MultiViewNestedUNet --num_classes=2
