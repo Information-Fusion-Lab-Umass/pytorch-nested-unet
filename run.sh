15f122c82dd59d93549408b2b799d2a960db676d
vbhave <vbhave@umass.edu>
2021-04-22 19:03:50 -0400

    Script file added.

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python train.py --dataset axial_full_train --arch NestedUNet --img_ext .png --mask_ext .png --num_classes=2 --input_w=96 --input_h=96
+python areaTrain.py --dataset long_axis_resize --arch NestedUNet --num_classes=2   --loss MSEAndBCEDiceLoss --lr=5e-9
