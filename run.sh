46194144337b5e8e3cf27992c442b7915849ebd3
erotondo <49079965+erotondo@users.noreply.github.com>
2021-05-08 15:04:10 -0400

    Fixed typo and updated run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python areaTrain.py --dataset long_axis_resize --arch NestedUNet --num_classes=2   --loss MSEAndBCEDiceLoss --lr=5e-9
+python areaTrain.py --dataset long_axis_resize --arch NestedUNet --num_classes=1  --loss MSEAndBCEDiceLoss --lr=5e-4 --name="long_axis_resize_with_lr_5e_4" --epoch=30
