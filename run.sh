4b74414780f6345ad955973cef198ff9df48c313
James Ko <jamesqko@gmail.com>
2021-05-09 20:17:40 -0400

    Fix secondViewPath

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python train.py --dataset short_axis_resize --dataset2 long_axis_resize --arch MultiViewNestedUNet --num_classes=2 --secondViewPath models/long_axis_single_view/model.pth
+python train.py --dataset short_axis_resize --dataset2 long_axis_resize --arch MultiViewNestedUNet --num_classes=2 --secondViewPath /mnt/nfs/work1/mfiterau/vbhave/segmentation/lvsc_dataset/multiview_lvsc/pytorch-nested-unet/models/long_axis_single_view/model.pth
